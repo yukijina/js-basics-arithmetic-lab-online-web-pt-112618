a532ba0c946260ed254aaf414dc51ffc82a8264d
yukijina <yukijina@gmail.com>
2019-06-08 11:27:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here
+let var1 = 2;
+let var2 = 31;
+const multiply = var1*var2;
+
+const random = Math.ceil(Math.random());
+
+const mod = 9%5;
+
+const max = Math.max(1,2,3,20,8,15);
